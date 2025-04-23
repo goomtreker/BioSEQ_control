e32555843a7b79fb33a653fec0b973fa7f0f81fa
goomtreker <anurovartemiy@gmail.com>
2025-04-23 19:27:34 +0300

    Add logging and commandline arguments for fastq filter

--- BioSEQ.py
+++ BioSEQ.py
@@ -1,7 +1,12 @@
+#!/usr/bin/env python
 from abc import ABC
 from Bio import SeqIO
 from Bio.SeqUtils import gc_fraction
 import numpy as np
+import argparse
+import logging
+import sys
+import os
 
 
 class BiologicalSequence(ABC):
@@ -71,13 +76,23 @@ class AminoAcidSequence(BiologicalSequence):
         return sum(self._charges.get(aa, 0) for aa in self.seq)
 
 
-# fastq filrator
 def make_bounds(limit: float | int | tuple) -> tuple:
     """Define limits, if it
     specified with one case"""
     if isinstance(limit, (int, float)):
         return (0, limit)
     return limit
+
+def setup_logging(log_file='fastq_filter.log'):
+    """My logger"""
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s | %(levelname)s | %(message)s',
+        handlers=[
+            logging.FileHandler(log_file),
+            logging.StreamHandler(sys.stdout)
+        ]
+    )
 
 
 # Function for filter fastq file format
@@ -87,13 +102,47 @@ def FilterFastQC(
         gc_bounds=(0, 100),
         len_bounds=(0, 2**32),
         quality_threshold=0) -> None:
+
     gc_bounds, length_bounds = make_bounds(gc_bounds), make_bounds(len_bounds)
-    with open(fastq_ouput, 'w') as out_handle:
-        for record in SeqIO.parse(fastq_input, 'fastq'):
-            seq_len = len(record.seq)
-            avg_quality = np.mean(record.letter_annotations['phred_quality'])
-            gc_content = gc_fraction(record.seq)
-            if gc_bounds[0] <= gc_content*100 <= gc_bounds[1] and \
-               length_bounds[0] <= seq_len <= length_bounds[1] and \
-               avg_quality >= quality_threshold:
-                SeqIO.write(record, out_handle, 'fastq')
+    try:
+        with open(fastq_ouput, 'w') as out_handle:
+            total_records = 0
+            passed_records = 0
+            for record in SeqIO.parse(fastq_input, 'fastq'):
+                total_records += 1
+                seq_len = len(record.seq)
+                avg_quality = np.mean(record.letter_annotations['phred_quality'])
+                gc_content = gc_fraction(record.seq)
+                if gc_bounds[0] <= gc_content*100 <= gc_bounds[1] and \
+                length_bounds[0] <= seq_len <= length_bounds[1] and \
+                avg_quality >= quality_threshold:
+                    passed_records += 1
+                    SeqIO.write(record, out_handle, 'fastq')
+                
+            logging.info(f"Total Records: {total_records}")
+            logging.info(f"Filtered Records: {passed_records}")
+    except FileNotFoundError:
+        logging.error(f"The File for input is not exist!")
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    setup_logging()
+
+    parser = argparse.ArgumentParser(description='Filter FASTQ files by GC content, length, and quality.')
+    parser.add_argument('-i', '--input', required=True, help='Input FASTQ file')
+    parser.add_argument('-o', '--output', required=True, help='Output FASTQ file')
+    parser.add_argument('--gc_bounds', nargs='+', type=float, default=[0, 100],
+                        help='GC bounds (min, max) if one value passed, the min will be equal 0')
+    parser.add_argument('--length', nargs='+', type=int, default=[0, 2**32],
+                        help='Length bounds (min, max) if one value passed, the min will be equal 0')
+    parser.add_argument('--quality', type=float, default=0,
+                        help='Minimum average Phred quality threshold')
+
+    args = parser.parse_args()
+
+    gc_bounds = make_bounds(args.gc_bounds)
+    len_bounds = make_bounds(args.length)
+    logging.info(f"The filteration wil bewith GC fraction={gc_bounds}, length range={len_bounds} and quality={args.quality}")
+
+    FilterFastQC(args.input, args.output, gc_bounds, len_bounds, args.quality)
